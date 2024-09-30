cc321bff7d887e096bdf98723695140516cbbe1e
Franco Durand <francoduve.25@gmail.com>
2024-09-29 21:36:50 -0500

    traducciones

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import './style.css'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
-
+import i18n from './i18n';
 
 const app = createApp(App);
 
+app.use(i18n);
 app.use(PrimeVue, {
     theme: {
         preset: Aura,
