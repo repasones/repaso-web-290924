8c33ba0e2c9303e31e7e69b154462220fa1387a7
Franco Durand <francoduve.25@gmail.com>
2024-09-29 21:29:45 -0500

    plantilla

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+
+
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false
+        }
+    }
+});
+
+
+app.mount('#app');
